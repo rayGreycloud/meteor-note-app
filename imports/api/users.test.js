464cdb93b5eb05c48a2a1a6afd34e8e4581ecbb8
rayGreycloud <rayGreycloud@gmail.com>
2017-03-26 06:44:38 -0700

    Add test for validateNewUser method

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -1,33 +1,18 @@
 import expect from 'expect';
 
-const add = (a, b) =>{
-  if (typeof b !== 'number') {
-    return a + a;
-  }
+import { validateNewUser } from './users';
 
-  return a + b;
-};
+describe('users', function () {
+  it('should return true for valid email', function () {
+    const testUser = {
+      emails: [
+        {
+          address: 'yoda@example.com'
+        }
+      ]
+    };
+    const res = validateNewUser(testUser);
 
-const square = (a) => a * a;
-
-describe('add function', function() {
-  it('should return the sum of two numbers', function() {
-    const res = add(11, 9);
-
-    expect(res).toBe(20);
-  });
-
-  it('should return double of a single number', function() {
-    const res = add(11);
-
-    expect(res).toBe(22);
-  });
-});
-
-describe('square function', function() {
-  it('should return the square of a number', function() {
-    const res = square(10);
-
-    expect(res).toBe(100);
+    expect(res).toBe(true);
   });
 });
