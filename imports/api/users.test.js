0b14db4f3914d8dbbb8a435153b9b05f8bf9a5c2
rayGreycloud <rayGreycloud@gmail.com>
2017-03-26 06:31:48 -0700

    Add expect for testing

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -1,3 +1,5 @@
+import expect from 'expect';
+
 const add = (a, b) =>{
   if (typeof b !== 'number') {
     return a + a;
@@ -12,26 +14,20 @@ describe('add function', function() {
   it('should return the sum of two numbers', function() {
     const res = add(11, 9);
 
-    if (res !== 20) {
-      throw new Error('Function did not return the expected value');
-    }
+    expect(res).toBe(20);
   });
 
   it('should return double of a single number', function() {
     const res = add(11);
 
-    if (res !== 22) {
-      throw new Error('Function add did not return the expected value');
-    }
+    expect(res).toBe(22);
   });
 });
 
 describe('square function', function() {
   it('should return the square of a number', function() {
     const res = square(10);
-  
-    if (res !==100) {
-      throw new Error('Function did not return the expected value');
-    }
+
+    expect(res).toBe(100);
   });
 });
