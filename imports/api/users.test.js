8e04adbc5e5ab89d9548a6e6703d750ac61b0409
rayGreycloud <rayGreycloud@gmail.com>
2017-03-26 06:52:20 -0700

    Change validateNewUser test to server only

--- imports/api/users.test.js
+++ imports/api/users.test.js
@@ -1,18 +1,22 @@
+import { Meteor } from 'meteor/meteor';
 import expect from 'expect';
 
 import { validateNewUser } from './users';
 
-describe('users', function () {
-  it('should return true for valid email', function () {
-    const testUser = {
-      emails: [
-        {
-          address: 'yoda@example.com'
-        }
-      ]
-    };
-    const res = validateNewUser(testUser);
+// Run test on server only
+if (Meteor.isServer) {
+  describe('users', function () {
+    it('should return true for valid email', function () {
+      const testUser = {
+        emails: [
+          {
+            address: 'yoda@example.com'
+          }
+        ]
+      };
+      const res = validateNewUser(testUser);
 
-    expect(res).toBe(true);
+      expect(res).toBe(true);
+    });
   });
-});
+};
