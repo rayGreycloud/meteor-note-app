517333d37d356b5eb6cc6419d2255ec0448d3566
rayGreycloud <rayGreycloud@gmail.com>
2017-03-27 06:52:52 -0700

    Add test for notes.remove

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -4,6 +4,18 @@ import { Notes } from './notes';
 
 if (Meteor.isServer) {
   describe('notes', function () {
+
+    beforeEach(function () {
+      Notes.remove({});
+      Notes.insert({
+        _id: 'testNoteId1',
+        title: 'Test Title',
+        body: 'Test Body',
+        updatedAt: 123,
+        userId: 'testUserId1'
+      });
+    });
+
     it('should insert new note', function () {
       const userId = 'testid';
       const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });
@@ -11,10 +23,16 @@ if (Meteor.isServer) {
       expect(Notes.findOne({ _id, userId })).toExist();
     });
 
-    it('should not insert if not authenticated', function () {
+    it('should not insert note if not authenticated', function () {
       expect(() => {
         Meteor.server.method_handlers['notes.insert']();
       }).toThrow();
+    });
+
+    it('should remove note', function () {
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' }, ['testNoteId1']);
+
+      expect(Notes.findOne({ _id: 'testNoteId1'})).toNotExist();
     });
   });
 }
