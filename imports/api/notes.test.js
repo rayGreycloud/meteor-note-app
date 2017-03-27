10df21b8a38821c50ca82745b81d51c12ed243ab
rayGreycloud <rayGreycloud@gmail.com>
2017-03-27 08:41:48 -0700

    Refactor Notes tests seed data

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -4,16 +4,17 @@ import { Notes } from './notes';
 
 if (Meteor.isServer) {
   describe('notes', function () {
+    const noteOne = {
+      _id: 'testNoteId1',
+      title: 'Test Title',
+      body: 'Test Body',
+      updatedAt: 123,
+      userId: 'testUserId1'
+    };
 
     beforeEach(function () {
       Notes.remove({});
-      Notes.insert({
-        _id: 'testNoteId1',
-        title: 'Test Title',
-        body: 'Test Body',
-        updatedAt: 123,
-        userId: 'testUserId1'
-      });
+      Notes.insert(noteOne);
     });
 
     it('should insert new note', function () {
@@ -30,21 +31,22 @@ if (Meteor.isServer) {
     });
 
     it('should remove note', function () {
-      Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' }, ['testNoteId1']);
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId }, [noteOne._id]);
 
-      expect(Notes.findOne({ _id: 'testNoteId1'})).toNotExist();
+      expect(Notes.findOne({ _id: noteOne._id})).toNotExist();
     });
 
     it('should not remove note if unauthenticated', function () {
       expect(() => {
-        Meteor.server.method_handlers['notes.remove'].apply({}, ['testNoteId1']);
+        Meteor.server.method_handlers['notes.remove'].apply({}, [noteOne._id]);
       }).toThrow();
     });
 
     it('should not remove note if invalid _id', function () {
       expect(() => {
-        Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' });
+        Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId });
       }).toThrow();
     });
-  });
+
+  }); // end describe
 }
