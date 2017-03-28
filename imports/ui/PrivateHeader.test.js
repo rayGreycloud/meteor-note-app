02ce3200fbadafdc083f5f508a97d30e9f171bd1
rayGreycloud <rayGreycloud@gmail.com>
2017-03-28 09:14:23 -0700

    Add PrivateHeader.test.js

---
+++ imports/ui/PrivateHeader.test.js
@@ -0,0 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import expect from 'expect';
+import { mount } from 'enzyme';
+
+import PrivateHeader from './PrivateHeader';
+
+if (Meteor.isClient) {
+  describe('PrivateHeader', function () {
+    it('should set button text to logout', function () {
+      const wrapper = mount( <PrivateHeader title="Test title"/> );
+      const buttonText = wrapper.find('button').text();
+
+      expect(buttonText).toBe('Logout');
+    });
+  });
+}
