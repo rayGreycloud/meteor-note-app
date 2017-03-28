7f9282c0aa6702e8306e0666b3be87485d72b592
rayGreycloud <rayGreycloud@gmail.com>
2017-03-28 06:47:15 -0700

    Test notes publication returns user notes

--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -6,15 +6,23 @@ if (Meteor.isServer) {
   describe('notes', function () {
     const noteOne = {
       _id: 'testNoteId1',
-      title: 'Test Title',
-      body: 'Test Body',
+      title: 'Test Title 1',
+      body: 'Test Body 1',
       updatedAt: 123,
       userId: 'testUserId1'
+    };
+    const noteTwo = {
+      _id: 'testNoteId2',
+      title: 'Test Title 2',
+      body: 'Test Body 2',
+      updatedAt: 123456789,
+      userId: 'testUserId2'
     };
 
     beforeEach(function () {
       Notes.remove({});
       Notes.insert(noteOne);
+      Notes.insert(noteTwo);
     });
 
     it('should insert new note', function () {
@@ -106,5 +114,15 @@ if (Meteor.isServer) {
         Meteor.server.method_handlers['notes.update'].apply({ userId: noteOne.userId });
       }).toThrow();
     });
+
+    it('should return a user notes', function () {
+      const res = Meteor.server.publish_handlers.notes.apply({ userId: noteOne.userId });
+      // Call fetch because above returns cursor
+      const notes = res.fetch();
+
+      expect(notes.length).toBe(1);
+      expect(notes[0]).toEqual(noteOne);
+    });
+
   }); // end describe
 }
