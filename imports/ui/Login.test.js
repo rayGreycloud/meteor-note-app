67762f8380563a2946fadabb78f2e1602529f1ef
rayGreycloud <rayGreycloud@gmail.com>
2017-03-29 08:33:53 -0700

    Add test of client loginWithPassword call

--- imports/ui/Login.test.js
+++ imports/ui/Login.test.js
@@ -18,5 +18,27 @@ if (Meteor.isClient) {
       wrapper.setState({ error: '' });
       expect(wrapper.find('p').length).toBe(0);
     });
+
+    it('should call loginWithPassword with form data', function () {
+      const email = 'darth@sith.com';
+      const password = 'password';
+      const spy = expect.createSpy();
+
+      const wrapper = mount(<Login loginWithPassword={spy}/>);
+
+      wrapper.ref('email').node.value = email;
+      wrapper.ref('password').node.value = password;
+      wrapper.find('form').simulate('submit');
+
+      expect(spy.calls[0].arguments[0]).toEqual({ email });
+      expect(spy.calls[0].arguments[1]).toBe(password);
+
+    });
+
+    it('should set loginWithPassword callback errors', function () {
+
+    });
+
+
   });
 }
