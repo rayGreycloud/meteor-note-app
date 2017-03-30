40d587078758e17d5fcd55acdf1200b289de338f
rayGreycloud <rayGreycloud@gmail.com>
2017-03-30 10:00:44 -0700

    Add proptypes to NoteListHeader

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -14,6 +14,10 @@ export const NoteListHeader = (props) => {
   );
 };
 
+NoteListHeader.propTypes = {
+  meteorCall: React.PropTypes.func.isRequired
+}
+
 export default createContainer(() => {
   return {
     meteorCall: Meteor.call
