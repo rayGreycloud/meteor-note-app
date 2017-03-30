6661b9be397b8d1e4cfb29a5f284606fbbc55434
rayGreycloud <rayGreycloud@gmail.com>
2017-03-30 06:44:45 -0700

    Add NoteList component

---
+++ imports/ui/NoteList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from  'meteor/react-meteor-data';
+
+import { Notes } from './../api/notes';
+
+export const NoteList = (props) => {
+  return (
+    <div>
+      NoteList { props.notes.length }
+    </div>
+  );
+};
+
+NoteList.propTypes = {
+  notes: React.PropTypes.array.isRequired
+};
+
+export default createContainer(() => {
+  Meteor.subscribe('notes');
+
+  return {
+    notes: Notes.find().fetch()
+  };
+}, NoteList);
