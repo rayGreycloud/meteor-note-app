35579d350fa723747a26b146835c9ea2df2507f7
rayGreycloud <rayGreycloud@gmail.com>
2017-03-30 07:07:27 -0700

    Add NoteListHeader component

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -3,10 +3,12 @@ import { Meteor } from 'meteor/meteor';
 import { createContainer } from  'meteor/react-meteor-data';
 
 import { Notes } from './../api/notes';
+import NoteListHeader from './NoteListHeader';
 
 export const NoteList = (props) => {
   return (
     <div>
+      <NoteListHeader />
       NoteList { props.notes.length }
     </div>
   );
