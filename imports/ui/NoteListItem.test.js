de642aa5ae5ea059677bd52dce8e639ba4915f4b
rayGreycloud <rayGreycloud@gmail.com>
2017-03-31 12:37:41 -0700

    Add test of click on NoteListItem

--- imports/ui/NoteListItem.test.js
+++ imports/ui/NoteListItem.test.js
@@ -17,8 +17,6 @@ if (Meteor.isClient) {
     });
 
     it('should render title and timestamp', function () {
-      const title = 'Note Title';
-      const updatedAt = 1490889476661; // 03/30/17 8:58a
       const wrapper = mount( <NoteListItem note={notes[0]} Session={Session}/> );
 
       expect(wrapper.find('h5').text()).toBe(notes[0].title);
@@ -31,5 +29,11 @@ if (Meteor.isClient) {
       expect(wrapper.find('h5').text()).toBe('Untitled note');
     });
 
+    it('should call set on click', function () {
+      const wrapper = mount( <NoteListItem note={notes[0]} Session={Session}/> );
+
+      wrapper.find('div').simulate('click');
+      expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id);
+    });
   });
 }
