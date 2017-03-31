1af719876f2de29e8a0f57fa60d0728d879df2ed
rayGreycloud <rayGreycloud@gmail.com>
2017-03-31 06:20:53 -0700

    Add component for empty item

---
+++ imports/ui/NoteListEmptyItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h5><em>'You have no notes'</em></h5>
+      <p>Create a note to get started</p>
+    </div>
+  );
+};
