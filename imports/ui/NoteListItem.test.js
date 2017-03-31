7c5fef40bc8f6a798b5d55fb9bc049bfc4b000b4
rayGreycloud <rayGreycloud@gmail.com>
2017-03-31 12:29:39 -0700

    Add Session spy to NoteListItem.test

--- imports/ui/NoteListItem.test.js
+++ imports/ui/NoteListItem.test.js
@@ -3,23 +3,30 @@ import expect from 'expect';
 import { Meteor } from 'meteor/meteor';
 import { mount } from 'enzyme';
 
-import NoteListItem from './NoteListItem';
+import { notes } from './../fixtures/fixtures';
+import { NoteListItem } from './NoteListItem';
 
 if (Meteor.isClient) {
   describe('NoteListItem', function () {
+    let Session;
+
+    beforeEach(() => {
+      Session = {
+        set: expect.createSpy()
+      };
+    });
+
     it('should render title and timestamp', function () {
       const title = 'Note Title';
       const updatedAt = 1490889476661; // 03/30/17 8:58a
-      const wrapper = mount( <NoteListItem note={{ title, updatedAt}}/> );
+      const wrapper = mount( <NoteListItem note={notes[0]} Session={Session}/> );
 
-      expect(wrapper.find('h5').text()).toBe(title);
+      expect(wrapper.find('h5').text()).toBe(notes[0].title);
       expect(wrapper.find('p').text()).toBe('3/30/17');
     });
 
     it('should set default title if none set', function () {
-      const title = '';
-      const updatedAt = 1490889476661; 
-      const wrapper = mount( <NoteListItem note={{ title, updatedAt}}/> );
+      const wrapper = mount( <NoteListItem note={notes[1]} Session={Session}/> );
 
       expect(wrapper.find('h5').text()).toBe('Untitled note');
     });
