f3b12a20005791aab777761d241043d87ceed464
rayGreycloud <rayGreycloud@gmail.com>
2017-03-31 07:13:16 -0700

    Add boilerplate for NoteListEmptyItem.test.js

---
+++ imports/ui/NoteListEmptyItem.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import expect from 'expect';
+import { Meteor } from 'meteor/meteor';
+import { mount } from 'enzyme';
+
+import { NoteList } from './NoteList';
+
+if (Meteor.isClient) {
+  describe('NoteListEmptyItem', function () {
+    it('should', function () {
+
+    });
+  });
+};
