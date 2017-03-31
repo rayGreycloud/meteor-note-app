595f479f68e7ca98c9910bc4aa61d500291fcdb6
rayGreycloud <rayGreycloud@gmail.com>
2017-03-31 08:57:15 -0700

    Add tests of NoteList rendering

--- imports/ui/NoteList.test.js
+++ imports/ui/NoteList.test.js
@@ -5,10 +5,37 @@ import { mount } from 'enzyme';
 
 import { NoteList } from './NoteList';
 
+const notes = [
+  {
+    _id: 'noteId1',
+    title: 'Test Note 1',
+    body: 'This is the body of Test Note 1',
+    updatedAt: 1234567890,
+    userId: 'userId1'
+  },
+  {
+    _id: 'noteId2',
+    title: 'Test Note 2',
+    body: 'This is the body of Test Note 2',
+    updatedAt: 1234567890,
+    userId: 'userId2'
+  }
+];
+
 if (Meteor.isClient) {
   describe('NoteList', function () {
-    it('should', function () {
+    it('should render NoteListItem for each note', function () {
+      const wrapper = mount( <NoteList notes={notes} />);
 
+      expect(wrapper.find('NoteListItem').length).toBe(2);
+      expect(wrapper.find('NoteListEmptyItem').length).toBe(0);
+    });
+
+    it('should render NoteListEmptyItem if zero notes', function () {
+      const wrapper = mount( <NoteList notes={[]} />);
+
+      expect(wrapper.find('NoteListItem').length).toBe(0);
+      expect(wrapper.find('NoteListEmptyItem').length).toBe(1);
     });
   });
 };
