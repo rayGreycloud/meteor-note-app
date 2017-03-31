cc6e3d086498b650565a2ad16196aea2bc11b207
rayGreycloud <rayGreycloud@gmail.com>
2017-03-31 12:26:07 -0700

    Revise data in fixtures

--- imports/fixtures/fixtures.js
+++ imports/fixtures/fixtures.js
@@ -2,15 +2,15 @@ export const notes = [
   {
     _id: 'noteId1',
     title: 'Test Note 1',
-    body: 'This is the body of Test Note 1',
-    updatedAt: 1234567890,
+    body: '',
+    updatedAt: 1490889476661,
     userId: 'userId1'
   },
   {
     _id: 'noteId2',
-    title: 'Test Note 2',
+    title: '',
     body: 'This is the body of Test Note 2',
-    updatedAt: 1234567890,
+    updatedAt: 1490889476661,
     userId: 'userId2'
   }
 ];
