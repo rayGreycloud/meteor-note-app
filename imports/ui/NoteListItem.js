f92ec53ee3079343335bcf57bdfafc9d22b9e9be
rayGreycloud <rayGreycloud@gmail.com>
2017-04-03 09:21:50 -0700

    Style list items on hover

--- imports/ui/NoteListItem.js
+++ imports/ui/NoteListItem.js
@@ -5,7 +5,7 @@ import { createContainer } from  'meteor/react-meteor-data';
 
 export const NoteListItem = (props) => {
   return (
-    <div onClick={() => {
+    <div className="item" onClick={() => {
       props.Session.set('selectedNoteId', props.note._id);
     }}>
       <h5>{ props.note.title || 'Untitled note' }</h5>
