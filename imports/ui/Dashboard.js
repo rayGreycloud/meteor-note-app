ac42f17da0ede604c062367008482675e9b4b47b
rayGreycloud <rayGreycloud@gmail.com>
2017-04-03 14:32:49 -0700

    Add styles for desktop size

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -7,7 +7,7 @@ import Editor from './Editor';
 const Dashboard = () => {
   return (
     <div>
-      <PrivateHeader title="Your Notes" />
+      <PrivateHeader title="Notes" />
       <div className="page-content">
         <div className="page-content__sidebar">
           <NoteList />
